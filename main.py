590b630633f20eb24036d3e0d61b621850623337
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:37:21 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Satrlar ro‘yxatidagi har bir matnni oxirgi so‘zini katta harflarda yozib chiqaring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
